972f6200c75e7bf1e7d2d91c37100d73094dd14a
SantINGx <santiagololgta777@gmail.com>
2026-08-15 17:08:20 -0600

    lenguajes

---
+++ src/main/java/com/gymcontrol/config/WebConfig.java
@@ -0,0 +1,44 @@
+package com.gymcontrol.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+
+        // Español como idioma predeterminado
+        resolver.setDefaultLocale(new Locale("es"));
+
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+
+        LocaleChangeInterceptor interceptor =
+                new LocaleChangeInterceptor();
+
+        // Permitirá cambiar idioma usando ?lang=en o ?lang=es
+        interceptor.setParamName("lang");
+
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
